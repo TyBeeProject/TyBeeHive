3eeaa890f0a7afcf7a6f470055c5bc0fda20ae5c
But2ene <xavier.duquesne@telecom-bretagne.eu>
2016-04-19 00:40:13 +0200

    create moistureCaptor.py

---
+++ captors-enabled/moistureCaptor.py
@@ -0,0 +1,16 @@
+
+
+class Captor():
+  id = 5
+  def Captor():
+    self.id = 5
+    
+    
+  def callback(self):
+    moisture = 0
+    #start communication with server
+    
+    return moisture
+    
+  def getiId(self):
+    return self.id
