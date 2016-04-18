598bdf1f19e373a1dfe67570930c504bd6c5a550
But2ene <xavier.duquesne@telecom-bretagne.eu>
2016-04-19 00:38:11 +0200

    create template for temperatureCaptor.py

---
+++ captors-enabled/temperatureCaptor.py
@@ -0,0 +1,16 @@
+
+
+class Captor():
+  id = 4
+  def Captor():
+    self.id = 4
+    
+    
+  def callback(self):
+    temperature = 0
+    #lance la communication avec le capteur
+    
+    return temperature
+    
+  def getiId(self):
+    return self.id
