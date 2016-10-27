a47a9ca211f2fc821c0ae40261fe4260829581bc
But2ene <xavier.duquesne@telecom-bretagne.eu>
2016-10-27 22:59:46 +0000

    moved captors to sensors available

---
+++ sensors-available/temperatureCaptor.py
@@ -0,0 +1,20 @@
+import Adafruit_Python_DHT
+
+pin = 14
+sensor = Adafruit_Pyhon_DHT.DHT22
+
+
+class Captor():
+  id = 1
+  def Captor():
+    self.id = 1
+    
+    
+  def callback(self):
+    moisture,temperature = Adafruit_Python_DHT.read(sensor,pin)
+    if temperature is None:
+      return 0
+    return temperature
+    
+  def getiId(self):
+    return self.id
