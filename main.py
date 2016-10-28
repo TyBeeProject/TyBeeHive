c88e58a59452016c52e01a52202b9b9198b0da30
But2ene <xavier.duquesne@telecom-bretagne.eu>
2016-10-28 12:38:52 +0200

    refactor sensor_enabled into a file
    permit to pass parameters to sensors

--- main.py
+++ main.py
@@ -61,33 +61,8 @@ def discover_sensors():
     And check them
     :return:
     """
-    sensorsObject = []
-    sensorsPath = []
-    for root, dirs, files in os.walk(conf.SENSORS_PATH):
-        for name in files:
-            if name[-3:] == ".py":
-                sensorsPath += [os.path.join(root, name)]
-
-    # Charge tout les capteurs et les teste
-    for captorPath in sensorsPath:
-        # On essaie d'importer le module du capteur
-        try:
-            captorLoader = SourceFileLoader('Sensor', captorPath).load_module()
-        except ImportError as e:
-            print('[FAIL] Loading :' + captorPath + ' issued :\n' + e)  # +errormessage
-        else:
-
-            # Test pour vérifier que le callback ne plante pas
-            try:
-                captorObject = captorLoader.Sensor()
-                captorObject.callback()
-                captorObject.getId()
-            except:
-                e = sys.exc_info()
-                print('[FAIL] Testing :' + captorPath + ' issued :\n' + str(e))
-            else:
-                sensorsObject.append(captorObject)
-    return sensorsObject
+    import sensor_enabled
+    return sensor_enabled.sensor_list
 
 
 def loop(sensorsObject):
---
+++ sensor_enabled.py
@@ -0,0 +1,7 @@
+# fichier de configuration listant le capteurs et leurs paramètres initiaux
+from sensors_available import WeightCaptor
+
+
+sensor_list = []
+
+sensor_list.append(WeightCaptor())
